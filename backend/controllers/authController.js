942398bda3058cf86c1067c830b929405cbd83ba
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-03-11 21:48:52 +0530

    refactor: improve code formatting and structure in authController; add token generation for login and OTP verification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,11 @@
-const User=require("../models/User")
-const sendEmail=require("../utils/emailSender")
+const User = require("../models/User")
+const jwt=require("jsonwebtoken")
+const sendEmail = require("../utils/emailSender")
+
+const generateAuthToken = function (id) {
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRES_IN });
+    return token;
+}
 
 exports.registerUser = async (req, res) => {
     try {
@@ -14,29 +20,30 @@ exports.registerUser = async (req, res) => {
             return res.status(400).json({ message: "Invalid email format" });
         }
 
-        const existingUser=await User.findOne({email});
+        const existingUser = await User.findOne({ email });
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
         }
-    
+
 
         const otp = Math.floor(100000 + Math.random() * 900000).toString();
         const otpExpiry = new Date(Date.now() + 10 * 60 * 1000);
 
         const user = await User.create({ username, email, password, otp, otpExpiry });
-        
-        try{
-          await sendEmail({
-            to:email,
-            subject:"Your OTP for ColdMail Generator",
-            text:`Your OTP  Code is ${otp}. It is valid for only 10 minutes only.`
-          })
-        }catch(error){
-            console.log({message:'Error sending OTP',error:error.message})
+
+        try {
+            await sendEmail({
+                to: email,
+                subject: "Your OTP for ColdMail Generator",
+                text: `Your OTP  Code is ${otp}. It is valid for only 10 minutes only.`
+            })
+        } catch (error) {
+            console.log({ message: 'Error sending OTP', error: error.message })
         }
 
-        return res.status(201).json({ message: "User registered successfully", user
+        return res.status(201).json({
+            message: "User registered successfully", user
         });
 
     } catch (err) {
@@ -45,45 +52,68 @@ exports.registerUser = async (req, res) => {
     }
 }
 
-exports.verifyOTP=async(req,res)=>{
-    try{
-        const {email,otp}=req.body;
+exports.verifyOTP = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
 
-        if(!email||!otp){
-            return res.status(400).json({message:"Email and OTP are required"})
+        if (!email || !otp) {
+            return res.status(400).json({ message: "Email and OTP are required" })
         }
 
-        const user=await User.findOne({email}).select("+otp +otpExpiry");
+        const user = await User.findOne({ email }).select("+otp +otpExpiry");
 
-        if(!user){
-            return res.status(400).json({message:"User not found"})
+        if (!user) {
+            return res.status(400).json({ message: "User not found" })
         }
 
-        if(user.otp!==otp){
-            return res.status(400).json({message:"Invalid OTP"})
+        if (user.otp !== otp.toString().trim()) {
+            return res.status(400).json({ message: "Invalid OTP" })
         }
 
-        if(user.otpExpiry<Date.now()){
-            return res.status(400).json({message:"OTP Expired"})
+        if (user.otpExpiry < Date.now()) {
+            return res.status(400).json({ message: "OTP Expired" })
         }
 
-        user.isVerified=true;
-        user.otp=undefined;
-        user.otpExpiry=undefined;
+        user.isVerified = true;
+        user.otp = undefined;
+        user.otpExpiry = undefined;
         await user.save();
 
-        return res.status(200).json({message:"Email verified successfully"})
+        const token = generateAuthToken(user._id);
+        return res.status(200).json({ token, message: "Email verified successfully" })
 
-    }catch(err){
-        console.error("Verification error:",err);
-        return res.status(500).json({message:"Internal server error",error:err.message})
+    } catch (err) {
+        return res.status(500).json({ message: "Error in verifying OTP", error: err.message })
     }
 }
 
-exports.loginUser=async(req,res)=>{
-    try{
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    }catch(error){
-        
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" })
+        }
+
+        const user = await User.findOne({ email }).select("+password +isVerified");
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" })
+        }
+
+        if (!user.isVerified) {
+            return res.status(400).json({ message: "User not Verified.Please verify your email first" })
+        }
+        const isMatch = await user.comparePassword(password)
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const token = generateAuthToken(user._id);
+        return res.status(200).json({ message: "Login successful",token, user: { username: user.username, email: user.email } });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error in logging in", error: err.message })
     }
 }
