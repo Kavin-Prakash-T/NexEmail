fc9ae3e792a5208672656fda3c2e19763bb48777
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-03-15 12:10:47 +0530

    feat: implement routing and loading state in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useAuth } from './context/authContext'
+import {BrowserRouter as Router, Routes, Route }  from "react-router-dom"
+import { Toaster } from 'react-hot-toast'
+import {LandingPage} from "./pages/LandingPage"
 
 function App() {
+
+  const { user, loading } = useAuth()
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <div>App</div>
+    <Router>
+      <Toaster position="top-right" />
+      <Routes>
+         <Route path="/" element={<LandingPage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
